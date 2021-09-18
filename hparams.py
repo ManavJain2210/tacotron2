ff14f78b981f2ba50f4b661aa3de5a44787ac7dc
ManavJain2210 <54030942+ManavJain2210@users.noreply.github.com>
2021-09-18 11:50:52 +0530

    changed hparam

--- hparams.py
+++ hparams.py
@@ -25,8 +25,8 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        validation_files='filelists/ljs_audio_text_val_filelist.txt',
+        training_files='../../input/hindi-data-tts/content/drive/MyDrive/Data/metadata_train.txt',
+        validation_files='../../input/hindi-data-tts/content/drive/MyDrive/Data/metadata_val.txt',
         text_cleaners=['english_cleaners'],
 
         ################################
