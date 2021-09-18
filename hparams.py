61007bb767849fd71a3e341ca31b45f26c71a321
ManavJain2210 <54030942+ManavJain2210@users.noreply.github.com>
2021-09-18 12:17:07 +0530

    Update hparams.py

--- hparams.py
+++ hparams.py
@@ -25,8 +25,8 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='../../input/hindi-data-tts/content/drive/MyDrive/Data/metadata_train.txt',
-        validation_files='../../input/hindi-data-tts/content/drive/MyDrive/Data/metadata_val.txt',
+        training_files='/kaggle/input/metadata-tacotron/metadata_train.txt',
+        validation_files='/kaggle/input/metadata-tacotron/metadata_val.txt',
         text_cleaners=['english_cleaners'],
 
         ################################
